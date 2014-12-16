1da3816319d4a5eb5057592c3ef4914796916470
Riley Shaw <rileyjshaw@gmail.com>
2014-12-16 05:09:12 -0500

    Update 2014-12-16T10:09:10.180Z

---
+++ main.js
@@ -0,0 +1,25 @@
+WebFont.load({
+  google: { families: ['Press Start 2P'] },
+  active: init
+});
+
+function init () {
+  var term = new TinyTerm(document.querySelector('.container'));
+
+  term.register('echo', {
+    fn: function () {
+      return Array.prototype.slice.call(arguments).join(' ');
+    },
+    desc: 'Write arguments to the standard output.'
+  });
+
+  term.register('js', {
+    fn: function () {
+      var args = Array.prototype.slice.call(arguments).join(' ');
+      return eval(args);
+    },
+    desc: 'Run arbitrary JS code.'
+  });
+
+  term.focus();
+}
