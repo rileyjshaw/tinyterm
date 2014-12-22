071737c0f96719c30bd484f5a6768064ef433370
Riley Shaw <rileyjshaw@gmail.com>
2014-12-21 21:09:25 -0500

    Update 2014-12-22T02:09:24.521Z

--- main.js
+++ main.js
@@ -4,14 +4,16 @@
 
     term.process = eval;
 
-    window.console.log = term.print.bind(term);
-    term.print('&nbsp;');
+    console.log = term.print.bind(term);
+    console.log('&nbsp;');
+    console.log('Property of World Incorporated, 1995.')
+    console.log('Created with <a href="//github.com/rileyjshaw/tinyterm">TinyTerm</a>.');
     term.focus();
   }
 
   if (typeof WebFont === 'object') {
     WebFont.load({
-      google: { families: ['Press Start 2P'] },
+      google: { families: ['VT323'] },
       active: init
     });
   } else {
